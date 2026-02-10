08ef4b1f0b74ea1975ccc82c8a4274dc05ad1146
jiho5755-maker <jiho5755@gmail.com>
2026-02-10 20:33:51 +0900

    fix(deploy): SQLite 마이그레이션 파일 삭제 (PostgreSQL 전환)

--- prisma/migrations/20260210000348_add_dashboard_indexes/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "AttendanceRecord_employeeId_date_idx" ON "AttendanceRecord"("employeeId", "date");
-
--- CreateIndex
-CREATE INDEX "AttendanceRecord_date_idx" ON "AttendanceRecord"("date");
-
--- CreateIndex
-CREATE INDEX "AttendanceRecord_isConfirmed_idx" ON "AttendanceRecord"("isConfirmed");
